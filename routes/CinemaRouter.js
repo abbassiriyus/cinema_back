9990a3faef1315aff0617309a48129ddfd333ec9
abbas <86982819+abbassiriyus@users.noreply.github.com>
2023-12-04 09:13:49 +0500

    as
    as

--- routes/CinemaRouter.js
+++ routes/CinemaRouter.js
@@ -69,7 +69,59 @@ rows[i].images.push(image_data[j])
       res.status(500).json({ error: err.message });
     }
   });
+  router.get('/api/v1/cinema/top', async (req, res) => {
+    try {
+      const { rows } = await pool.query('SELECT * FROM cinema ORDER BY more_loking');
+    var image=await pool.query('SELECT * FROM image_cinema ORDER BY id')
+    var image_data=image.rows
+
+    var mark=await pool.query('SELECT * FROM mark ORDER BY id')
+    var mark_data=mark.rows
+    var janr_c=await pool.query('SELECT * FROM janr_cinema ORDER BY id')
+    var janr_data_c=janr_c.rows
+
+    var janr=await pool.query('SELECT * FROM janr ORDER BY id')
+    var janr_data=janr.rows
+
+    for (let i = 0; i < janr_data_c.length; i++) {
+for (let j = 0; j < janr_data.length; j++) {
+if(janr_data_c[i].janr_id==janr_data[j].id){
+  janr_data_c[i].title=janr_data[j].title
+} }
+      
+    }
+
+for (let i = 0; i < rows.length; i++) {
+  rows[i].mark=10
+  rows[i].mark_org=false
+
+  rows[i].janrlar=[]
+  rows[i].images=[]
+ for (let j = 0; j < image_data.length; j++) {
+ if(rows[i].id===image_data[j].cinema_id){
+rows[i].images.push(image_data[j])
+ }
+
+ }
+ for (let j = 0; j < janr_data_c.length; j++) {
+  if(rows[i].id===janr_data_c[j].cinema_id){
+ rows[i].janrlar.push(janr_data_c[j])
+  }
+ 
+  }
+ for (let j = 0; j < mark_data.length; j++) {
+  if(rows[i].id===mark_data[j].cinema_id){
+    rows[i].mark=(rows[i].mark+mark_data[j].title)/2
+    rows[i].mark_org=true
+  }
+  }
   
+}
+      res.json(rows);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
   router.get('/api/v1/cinema/:id', async (req, res) => {
     try {
       const { id } = req.params;
