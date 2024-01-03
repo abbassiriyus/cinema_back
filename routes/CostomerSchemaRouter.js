9b31c08c10a18917d6be09a246cb212176134603
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-01-03 16:23:33 +0500

    sfd
    fds

--- routes/CostomerSchemaRouter.js
+++ routes/CostomerSchemaRouter.js
@@ -5,15 +5,14 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const pool = require("../db.js");
 const { isBetweenStartAndEnd,sendEmail } = require('../middleware/Auth.js');
-
+const nodemailer=require('nodemailer')
 const router = express.Router();
 // router.use(bodyParser.json());
 
 const SECRET = process.env.TOKEN_SECRET;
 
-router.post('/send-email', async (req, res) => {
-  const { email } = req.body;
-
+router.post('/send-message', async (req, res) => {
+  const { email,ism,mavzu,xabar } = req.body;
   try {
     // PostgreSQL veritabanından kullanıcıyı sorgula
     const query = 'SELECT * FROM users WHERE email = $1';
@@ -26,12 +25,78 @@ router.post('/send-email', async (req, res) => {
     const user = result.rows[0];
     const {  password } = user;
     // E-posta gönderme işlemini gerçekleştirin
-    sendEmail(email,password);
-
-    res.sendStatus(200);
+ const transporter = nodemailer.createTransport({
+      pool: true,
+     service: "gmail",
+     auth: {
+        user: "uzdub.group@gmail.com",
+        pass: 'fbcgnvqfbmocflcm',
+     },
+      tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false
+    },
+    });
+    const mailOptions = {
+      from: 'uzdub.group@gmail.com',
+      to: 'uzdub.group@gmail.com',
+      subject: 'aloqaga chiqing',
+      text: `sizga yuborilgan email: ${email};ismi:${ism},maqsadi:${xabar},mavzusi:${mavzu}`,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        res.status(400).json(error)
+      } else {
+      res.status(200).json(info)
+      }
+    });
   } catch (error) {
     console.error('Veritabanı hatası:', error);
-    res.sendStatus(500);
+    res.status(500).json({err:error.message});
+  }
+});
+router.post('/send-email', async (req, res) => {
+  const { email } = req.body;
+  try {
+    // PostgreSQL veritabanından kullanıcıyı sorgula
+    const query = 'SELECT * FROM users WHERE email = $1';
+    const result = await pool.query(query, [email]);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: 'Kullanıcı bulunamadı.' });
+    }
+
+    const user = result.rows[0];
+    const {  password } = user;
+    // E-posta gönderme işlemini gerçekleştirin
+ const transporter = nodemailer.createTransport({
+      pool: true,
+     service: "gmail",
+     auth: {
+        user: "uzdub.group@gmail.com",
+        pass: 'fbcgnvqfbmocflcm',
+     },
+      tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false
+    },
+    });
+    const mailOptions = {
+      from: 'uzdub.group@gmail.com',
+      to: email,
+      subject: 'Parolanızı alın',
+      text: `sizning parolingiz: ${password}`,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        res.status(400).json(error)
+      } else {
+      res.status(200).json(info)
+      }
+    });
+  } catch (error) {
+    console.error('Veritabanı hatası:', error);
+    res.status(500).json({err:error.message});
   }
 });
 
